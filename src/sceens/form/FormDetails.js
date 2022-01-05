b71349fe134d7012bd0601848b1298f8d450081e
azoom-duong-phuc-thang <duong.phuc.thang@azoom.jp>
2022-01-05 11:36:26 +0700

    Fix loading load

--- src/sceens/form/FormDetails.js
+++ src/sceens/form/FormDetails.js
@@ -10,14 +10,17 @@ import {
   ScrollView,
   TouchableOpacity,
   Alert,
+  TextInput,
 } from "react-native";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { BarCodeScanner } from "expo-barcode-scanner";
 import { useSelector, useDispatch } from "react-redux";
 import { Modal, Portal, Button, Provider } from "react-native-paper";
 import RadioButtonRN from "radio-buttons-react-native";
 import Checkbox from "expo-checkbox";
 import { createForms, deleteForm } from "@api";
 import { DeleteFormAction } from "../../redux/action";
-import { getForm } from "../../../api";
+import { getForm, submitForm } from "../../../api";
 import _ from "lodash";
 
 var nameField = "";
@@ -35,7 +38,11 @@ export default function formDetails({ navigation, route }) {
   const [dataForm, setDataForm] = useState({});
   const [isDelete, setIsDete] = useState(false);
   const [isUpdate, setIsUpdate] = useState(false);
-  const [hasMore,setHasMore]=useState(false)
+  const [hasMore, setHasMore] = useState(false);
+  const [formData, setFormData] = useState(dataForm.formInput);
+  const [show, setShow] = useState(false);
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
   const [groupRole, setGroupRole] = useState(
     groups.map((e) => {
       return {
@@ -47,10 +54,58 @@ export default function formDetails({ navigation, route }) {
       };
     })
   );
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
   const onDeleteForm = async () => {
     try {
       await deleteForm(dataForm.id, token);
       alert("Bạn xoá thành công");
+      navigation.replace("ListForm");
+    } catch (error) {
+      alert("Bạn không được cấp quyền xoá ");
+    }
+  };
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  const onSubmit = async () => {
+    try {
+      console.log(formData);
+      const body = {
+        name: dataForm.name,
+        formInput: formData,
+      };
+      const data = {
+        data: JSON.stringify(body),
+      };
+      await submitForm(token, data);
+      alert("Bạn gửi  thành công");
       navigation.replace("ListForm");
     } catch (error) {
       alert("Bạn không được cấp quyền xoá ");
@@ -69,7 +124,6 @@ export default function formDetails({ navigation, route }) {
       if (checkDel) {
         setIsDete(true);
       }
-      console.log(checkUpdate, checkDel);
     } catch (error) {}
   };
   const renderStatusBar = () => {
@@ -111,6 +165,13 @@ export default function formDetails({ navigation, route }) {
       </View>
     );
   };
+  const onSubmitEdit = (e, index) => {
+    let newdataForm = JSON.parse(JSON.stringify(dataForm.formInput));
+    console.log(e, index);
+    newdataForm.splice(index, 1);
+    newdataForm.push(e);
+    setFormData(newdataForm);
+  };
 
   const formType = [
     {
@@ -135,9 +196,17 @@ export default function formDetails({ navigation, route }) {
           style={StyleSheet.absoluteFillObject}
           blurRadius={50}
         />
+
         {renderStatusBar()}
+
         <SafeAreaView style={styles.container}>
           <Header navigation={navigation}></Header>
+          {scanned && (
+            <BarCodeScanner
+              onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+              style={{ flex: 1 }}
+            />
+          )}
           {!_.isEmpty(dataForm) && (
             <View style={{ flex: 1, paddingHorizontal: 5 }}>
               <View>
@@ -166,46 +235,139 @@ export default function formDetails({ navigation, route }) {
               <ScrollView style={{ flex: 1 }}>
                 <View>
                   {dataForm.formInput.map((e, index) => (
-                    <View
-                      style={{
-                        backgroundColor: "white",
-                        flexDirection: "row",
-                        justifyContent: "space-between",
-                        margin: 10,
-                        padding: 10,
-                        borderRadius: 10,
-                      }}
-                    >
-                      <Text style={{ fontSize: 20 }}>{e?.label}</Text>
-                      <Text style={{ fontSize: 20, color: "blue" }}>
-                        {e?.type}
-                      </Text>
-                    </View>
+                    <>
+                      <View
+                        style={{
+                          backgroundColor: "white",
+                          flexDirection: "row",
+                          justifyContent: "space-between",
+                          margin: 10,
+                          padding: 10,
+                          borderRadius: 10,
+                        }}
+                      >
+                        <Text style={{ fontSize: 20 }}>{e?.label}</Text>
+                        <Text style={{ fontSize: 20, color: "blue" }}>
+                          {e?.type}
+                        </Text>
+                      </View>
+                      <View
+                        style={{
+                          flexDirection: "row",
+                          padding: 10,
+                          borderRadius: 10,
+                          alignItems: "center",
+                        }}
+                      >
+                        <Text style={{ color: "white" }}>Value:</Text>
+                        {e.type == "Number" && (
+                          <TextInput
+                            placeholder="Value"
+                            onChangeText={(text) => {
+                              e.value = text;
+                            }}
+                            style={styles.textView}
+                            keyboardType="numeric"
+                            returnKeyType="done"
+                            onSubmitEditing={(e, index) =>
+                              onSubmitEdit(e, index)
+                            }
+                          ></TextInput>
+                        )}
+                        {e.type == "Text" && (
+                          <TextInput
+                            placeholder="Value"
+                            onChangeText={(text) => {
+                              e.value = text;
+                            }}
+                            style={styles.textView}
+                            returnKeyType="done"
+                            onSubmitEditing={(e, index) =>
+                              onSubmitEdit(e, index)
+                            }
+                          ></TextInput>
+                        )}
+                        {e.type == "Date" && (
+                          <>
+                          <TextInput
+                            placeholder="Value"
+                            onChangeText={(text) => {
+                              e.value = text;
+                            }}
+                            style={styles.textView}
+                            returnKeyType="done"
+                            onTouchStart={()=>{setShow(true)
+                            }}
+                            onSubmitEditing={(e, index) =>
+                              onSubmitEdit(e, index)
+                            }
+                          ></TextInput>
+                          {/* {show && (
+                            <DateTimePicker
+                              testID="dateTimePicker"
+                              value={date}
+                              mode={mode}
+                              is24Hour={true}
+                              display="default"
+                              onChange={onChange}
+                            />
+                          )} */}
+                          </>
+                        )}
+                        {e.type == "BarCode" && (
+                          <>
+                            <TextInput
+                              placeholder="Value"
+                              onChangeText={(text) => {
+                                e.value = text;
+                              }}
+                              style={styles.textView}
+                              returnKeyType="done"
+                              onSubmitEditing={(e, index) =>
+                                onSubmitEdit(e, index)
+                              }
+                            ></TextInput>
+                            {/* <TouchableOpacity
+                              onPress={() => {
+                                const data = scanned;
+                                setScanned(!data);
+                              }}
+                            >
+                              <Image
+                                style={{ height: 60, margin: 20 }}
+                                source={require("@assets/imageforms.png")}
+                                resizeMode="contain"
+                              />
+                            </TouchableOpacity> */}
+                          </>
+                        )}
+                      </View>
+                    </>
                   ))}
                 </View>
               </ScrollView>
             </View>
           )}
-           <TouchableOpacity
-                style={styles.buttonSend}
-                onPress={() =>
-                  Alert.alert("Bạn chắc chắn muốn xoá biểu mẫu", "", [
-                    {
-                      text: "Cancel",
-                      onPress: () => console.log("Cancel Pressed"),
-                      style: "cancel",
-                    },
-                    { text: "OK", onPress: () => onDeleteForm() },
-                  ])
-                }
-              >
-                <Text style={styles.loginText}>Send</Text>
-                <Image
-                  style={{ marginLeft: 5 }}
-                  source={require("@assets/trash1.png")}
-                  resizeMode="contain"
-                />
-              </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.buttonSend}
+            onPress={() =>
+              Alert.alert("Bạn chắc chắn muốn gửi biểu mẫu", "", [
+                {
+                  text: "Cancel",
+                  onPress: () => console.log("Cancel Pressed"),
+                  style: "cancel",
+                },
+                { text: "OK", onPress: () => onSubmit() },
+              ])
+            }
+          >
+            <Text style={styles.loginText}>Send</Text>
+            <Image
+              style={{ marginLeft: 5 }}
+              source={require("@assets/send.png")}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
           <View
             style={{ flexDirection: "row", justifyContent: "space-around" }}
           >
@@ -245,9 +407,7 @@ export default function formDetails({ navigation, route }) {
                 />
               </TouchableOpacity>
             )}
-            
           </View>
-         
         </SafeAreaView>
       </SafeAreaView>
     </Provider>
@@ -264,7 +424,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginHorizontal: 10,
   },
-  buttonSend:{
+  buttonSend: {
     marginVertical: 20,
     marginHorizontal: 10,
     // width: Constant.width7Unit,
@@ -303,5 +463,13 @@ const styles = StyleSheet.create({
   },
   checkbox: {
     alignSelf: "center",
+  },
+  textView: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 10,
+    height: 50,
+    marginLeft: 10,
+    borderRadius: 10,
   },
 });
